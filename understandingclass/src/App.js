6f8ec548b2d3ee896c4b0a2bca271b3f0090dc0c
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2021-10-09 01:47:01 +0200

    completed

--- understandingclass/src/App.js
+++ understandingclass/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {lat: null, errorMessage: '' };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude}),
+      (error) => this.setState({ errorMessage: error.message})
+    );
+  }
+  render() {
+    
 
-function App() {
-  return (
-    <h1>Hello there</h1>
-  );
+      if(this.state.errorMessage && !this.state.lat){
+        return <div>Error:{this.state.errorMessage}</div>;
+      }
+      if(!this.state.errorMessage && this.state.lat){
+        return <div>Latitude:{this.state.lat}</div>;
+      }
+      return <div>Loading!</div>
+  }
 }
 
 export default App;
