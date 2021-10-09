345c2f3e3ffe14befb7b1e9572852cc2b52f76ce
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2021-10-09 13:07:51 +0200

    added more functionality

--- understandingclass/src/App.js
+++ understandingclass/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner'
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {lat: null, errorMessage: '' };
+  state = {lat: null, errorMessage: '' };
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude}),
       (error) => this.setState({ errorMessage: error.message})
     );
+  }
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered')
   }
   render() {
     
@@ -16,9 +19,9 @@ class App extends React.Component {
         return <div>Error:{this.state.errorMessage}</div>;
       }
       if(!this.state.errorMessage && this.state.lat){
-        return <div>Latitude:{this.state.lat}</div>;
+        return <SeasonDisplay lat={this.state.lat}/>
       }
-      return <div>Loading!</div>
+      return <Spinner message="Please accept location request"/>;
   }
 }
 
--- understandingclass/src/SeasonDisplay.js
+++ understandingclass/src/SeasonDisplay.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import './SeasonDisplay.css'
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer: {
+        text: "lets's hit the beach",
+        iconName: " massive sun icon"
+    },
+    winter: {
+        text: "Burr It's cold",
+        iconName: "icon-right massive snowflake icon"
+    }
+}
+
+const getSeason = (lat, month) => {
+    if (month>2 && month<9){
+        return lat>0?'summer':'winter';
+    } else {
+        return lat>0? 'winter':'summer';
+    }
+}
+ 
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season]
     return (
-        <div>
-            Season Display
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left ${iconName}`}/>
+          <h1>{text}</h1>
+          <i className={`icon-right ${iconName}`}/>
         </div>
     )
 }
