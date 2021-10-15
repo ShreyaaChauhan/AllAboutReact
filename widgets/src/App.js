ff1cb0a3647351487ea9cdb5faa0ede3604dcbd9
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2021-10-16 00:19:40 +0200

    today's work

---
+++ widgets/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Accordion from "./Components/Accordion";
+import Search from "./Components/Search";
+import Dropdown from "./Components/Dropdown";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is front end javascript framework",
+  },
+  {
+    title: "Why se React?",
+    content: "React is favorite JS Library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+const options = [
+  {
+    label: "The Color red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "Green",
+  },
+  {
+    label: "A shade of blue",
+    value: "Blue",
+  },
+];
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default App;
